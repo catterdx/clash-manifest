c76bb80f5309bf913159f18827aee82668811949
catterdx <58603950+catterdx@users.noreply.github.com>
2020-06-02 14:49:53 +0800

    Update allping.sh
    try add allping

--- test_code/allping.sh
+++ test_code/allping.sh
@@ -1,1 +1,1 @@
-wget -N --no-check-certificate https://raw.githubusercontent.com/catterdx/91test/master/test_code/ZPing.py && chmod a+rx ./91test/ZPing.py && python ./91test/ZPing.py
+wget -N --no-check-certificate https://raw.githubusercontent.com/catterdx/91test/master/test_code/ZPing.py && chmod a+rx ZPing.py && python ZPing.py
