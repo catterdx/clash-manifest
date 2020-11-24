2cabec03c35307fede02ec1063b00ed19a7fb7dd
catterdx <58603950+catterdx@users.noreply.github.com>
2020-11-24 19:32:14 +0800

    python3 change

--- test_code/allping.sh
+++ test_code/allping.sh
@@ -1,1 +1,1 @@
-wget -N --no-check-certificate https://raw.githubusercontent.com/catterdx/91test/master/test_code/ZPing.py && chmod a+rx ZPing.py && python ZPing.py
+wget -N --no-check-certificate https://raw.githubusercontent.com/catterdx/91test/master/test_code/ZPing.py && chmod a+rx ZPing.py && python3 ZPing.py
